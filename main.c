85bccdb19fc7079fe666c44844e7bc2f8ab79f3f
Anaïs Mfaboum <anais.mfaboum_mbiafu@edu.devinci.fr>
2021-12-14 18:02:11 +0100

    main.c

--- main.c
+++ main.c
@@ -1,5 +1,33 @@
 int main (){
 	for (int i=1; i<11; i+1){
-		printf("%d/n", i);
-	}
+		printf ("%d/n",i);
+	}#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+        srand (time(NULL));
+        int number_to_guess=rand()%100+1;
+        int guess=0;
+        printf("I have in mind a number in between 1 and 100, can you find it?\n");
+        while(guess!=number_to_guess){
+                scanf("%d", &guess);
+                if(guess>=1 && guess<=100){
+                        if(guess>number_to_guess){
+                                printf("The number to guess is lower\n");
+                        }
+                        else if(guess<number_to_guess){
+                                printf("The number to guess is higher\n");
+                        }
+                }
+                else {
+                        printf("Invalude input");
+                        return 1;
+                }
+        }
+        printf("You just found the number, it was indeed %d\n, guess");
+        return 0;
+}
+	return 0;
 }
